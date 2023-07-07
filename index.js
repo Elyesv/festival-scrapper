2de6dc74721e25eacb5f7e31963c2c5d08a88771
Elyesv <elyesvoisin52@gmail.com>
2023-07-07 14:45:48 +0200

    ADD get festival by region + ci

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function getFestivalByRegion(region) {
+import fetch from 'node-fetch'
+
+export const getFestivalByRegion = async (region) => {
     let encodedRegion = encodeURIComponent(region);
     let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&refine.region=" + encodedRegion
-    return url
+    
+    const response = await fetch(url)
+
+    return response.json()
 }
 
-export default getFestivalByRegion;
