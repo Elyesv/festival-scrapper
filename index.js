278cedededbbd8087d46bcca8e1756745bd4dfa0
Elyesv <elyesvoisin52@gmail.com>
2023-07-07 15:54:46 +0200

    FIX number of value show

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 export const getFestivalByRegion = async (region) => {
     let encodedRegion = encodeURIComponent(region);
-    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&refine.region=" + encodedRegion
+    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&rows=9999&refine.region=" + encodedRegion
     
     const response = await fetch(url)
 
@@ -10,7 +10,7 @@ export const getFestivalByRegion = async (region) => {
 }
 
 export const getFestivalByDepartment = async (departement) => {
-    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&refine.departement=" + departement
+    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&rows=9999&refine.departement=" + departement
     
     const response = await fetch(url)
 
@@ -19,7 +19,7 @@ export const getFestivalByDepartment = async (departement) => {
 
 export const getFestivalByTheme = async (theme) => {
     let encodedTheme = encodeURIComponent(theme);
-    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&refine.complement_domaine=" + encodedTheme
+    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&rows=9999&refine.complement_domaine=" + encodedTheme
     
     const response = await fetch(url)
 
@@ -28,7 +28,7 @@ export const getFestivalByTheme = async (theme) => {
 
 export const getFestivalByRegionAndDepartement = async (region, departement) => {
     let encodedRegion = encodeURIComponent(region);
-    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&refine.region="+ encodedRegion +"&refine.departement=" + departement
+    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&q=&rows=9999&refine.region="+ encodedRegion +"&refine.departement=" + departement
 
     const response = await fetch(url)
 
@@ -36,8 +36,9 @@ export const getFestivalByRegionAndDepartement = async (region, departement) => 
 }
 
 export const getFestivalById = async (id) => {
-    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&refine.recordId=" + id
+    let url = "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&q=&rows=9999&refine.recordId=" + id
     const response = await fetch(url)
 
     return response.json()
 }
+
